474ca2e26d1304ef657cd5129a3791d66bde7d41
Mostafa <mostafaaboeldahab20@gmail.com>
2025-11-28 13:03:51 +0200

    Fixed SSR problem for Typed.js and Routing

--- src/app/Feature/Components/home/home.component.ts
+++ src/app/Feature/Components/home/home.component.ts
@@ -1,31 +1,38 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import Typed from 'typed.js';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements AfterViewInit {
-ngAfterViewInit(): void {
-    const options = {
-      strings: [
-        "Sport Events",
-        "Concerts",
-        "Conferences",
-        "Courses",
-        "Workshops",
-        "Parties",
-        "Theater"
-      ],
-      typeSpeed: 60,
-      backSpeed: 10,
-      backDelay: 2000, // Wait 2 seconds before deleting
-      loop: true
-    };
+  
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
-    new Typed('#typedElement', options);
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const options = {
+        strings: [
+          "Sport Events",
+          "Concerts",
+          "Conferences",
+          "Courses",
+          "Workshops",
+          "Parties",
+          "Theater"
+        ],
+        typeSpeed: 60,
+        backSpeed: 10,
+        backDelay: 2000, // Wait 2 seconds before deleting
+        loop: true
+      };
+
+      new Typed('#typedElement', options);
+    }
   }
 }
