05b0981c42afb0fd267dfd2dbdbf6cf95b5d9ad8
gannatullah <gannaamgadqwe@gmail.com>
2025-11-30 13:23:00 +0200

    Added the rest of the components to the navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,4 +17,32 @@ export const routes: Routes = [
         (c) => c.EventsComponent
       ),
   },
+  {
+    path: 'mybookings',
+    loadComponent: () =>
+      import('./Feature/Components/mybookings/mybookings.component').then(
+        (c) => c.MybookingsComponent
+      ),
+  },
+  {
+    path: 'profile',
+    loadComponent: () =>
+      import('./Feature/Components/profile/profile.component').then(
+        (c) => c.ProfileComponent
+      ),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./Core/Components/login/login.component').then(
+        (c) => c.LoginComponent
+      ),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./Core/Components/register/register.component').then(
+        (c) => c.RegisterComponent
+      ),
+  },
 ];
