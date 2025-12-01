98b2ab83f7a93a22f0938f1e144be4700d27c10f
mennamr <mennamrahmed@gmail.com>
2025-12-01 12:16:40 +0200

    register form basic elements HTML and TS

--- Frontend/src/app/Core/Components/register/register.component.ts
+++ Frontend/src/app/Core/Components/register/register.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import { count } from 'console';
+import { first } from 'rxjs';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  registerForm: FormGroup=new FormGroup({
+    firstName: new FormControl(),
+    lastName: new FormControl(),
+    email: new FormControl(),
+    password: new FormControl(),
+    confirmPassword: new FormControl(),
+    mobileNumber: new FormControl(),
+    dateOfBirth: new FormControl(),
+    country: new FormControl(),
+    city: new FormControl(),
+    nationality: new FormControl()
+
+  });
+
+  register(){
+    console.log(this.registerForm);
+  }
+
 
 }
