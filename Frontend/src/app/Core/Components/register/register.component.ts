8913f9cd4d8c82293741f2a3ea5d131b718d72e6
mennamr <mennamrahmed@gmail.com>
2025-12-07 18:23:20 +0200

    added date validation to birthdate input

--- Frontend/src/app/Core/Components/register/register.component.ts
+++ Frontend/src/app/Core/Components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
 import { count } from 'console';
 import { first } from 'rxjs';
 @Component({
@@ -10,6 +10,11 @@ import { first } from 'rxjs';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+today = new Date();
+endRange=new Date(this.today.getFullYear(), this.today.getMonth(), this.today.getDate()).toISOString().split('T')[0];
+maxBirthdate = new Date(this.today.getFullYear() - 18, this.today.getMonth(), this.today.getDate()).toISOString().split('T')[0];
+minBirthdate = new Date(this.today.getFullYear() - 100, this.today.getMonth(), this.today.getDate()).toISOString().split('T')[0];
+
 //control names must match the json keys in backend
   registerForm: FormGroup=new FormGroup({
     firstName: new FormControl(null, [Validators.required]),
@@ -18,7 +23,7 @@ export class RegisterComponent {
     password: new FormControl(null, [Validators.required]),
     confirmPassword: new FormControl(null, [Validators.required]),
     mobileNumber: new FormControl(null, [Validators.required]),
-    dateOfBirth: new FormControl(null, [Validators.required]),
+    dateOfBirth: new FormControl(null, [Validators.required, this.validateAge()]),
     country: new FormControl(null, [Validators.required]),
     city: new FormControl(null, [Validators.required]),
     nationality: new FormControl(null, [Validators.required])
@@ -45,6 +50,20 @@ export class RegisterComponent {
     return {passwordMismatch:true};
   }
 }
+validateAge(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const birthDate = new Date(control.value);
+    if (!birthDate) return null;
 
+    const today = new Date();
+
+    const minDate = new Date(today.getFullYear() - 100, today.getMonth(), today.getDate()); // 100 years ago
+    const maxDate = new Date(today.getFullYear() - 18, today.getMonth(), today.getDate());  // 18 years ago
+
+    const valid = birthDate >= minDate && birthDate <= maxDate;
+
+    return valid ? null : { ageRange: { minAge: 18, maxAge: 100 } };
+  };
+}
 
 }
